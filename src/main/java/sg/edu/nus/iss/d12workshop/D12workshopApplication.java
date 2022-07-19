cd6e0003b25049e333020713f9d2acba6167c3cb
zawan1997 <nawaz.shss@gmail.com>
2022-07-19 09:56:05 +0800

    initial commit

---
+++ src/main/java/sg/edu/nus/iss/d12workshop/D12workshopApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.d12workshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class D12workshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(D12workshopApplication.class, args);
+	}
+
+}
