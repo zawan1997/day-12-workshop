85a5bdc6dee2e4f7a6eed6d36dea93f5fe50310f
zawan1997 <nawaz.shss@gmail.com>
2022-07-19 11:31:01 +0800

    cannot show calendar details

--- src/main/java/sg/edu/nus/iss/d12workshop/IndexResource.java
+++ src/main/java/sg/edu/nus/iss/d12workshop/IndexResource.java
@@ -1,22 +1,24 @@
 package sg.edu.nus.iss.d12workshop;
 
+import java.util.Calendar;
+import java.util.Date;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-//Controller class build to direct people to the correct resource
 @Controller
-
-// to call the Controller class for this HTML resource, must use this path. The
-// resource next to it is the default
 @RequestMapping(path = { "/indexresource", "/indexresource.html" })
 public class IndexResource {
 
-    @GetMapping(produces = { "text.html" })
+    @GetMapping(produces = { "text/html" })
 
     //Without the Model model, the Controller will just be static not dynamic 
     public String index(Model model) {
+        model.addAttribute("currTime", (new Date()).toString());
+        Calendar cal = Calendar.getInstance();
+        model.addAttribute("currHour", cal.get(Calendar.HOUR_OF_DAY));
         return "indexresource";
         //create the html page in the templates folder with the same name
     }
